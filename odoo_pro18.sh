ec522c076ca4ba73559e5da1d32bde8bfa251e7a
Mahmoud Abdel Latif <mah008@me.com>
2019-12-10 11:47:05 +0200

    Update odoo_pro18.sh

--- odoo_pro18.sh
+++ odoo_pro18.sh
@@ -42,7 +42,11 @@ echo -e "\n---- Update Server ----"
 sudo apt-get update
 sudo apt-get upgrade -y
 apt install zip
+echo "----------------------------localization-------------------------------"
 
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
 
 #--------------------------------------------------
 # Install PostgreSQL Server
