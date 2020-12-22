2e24cd85e22469d9db2ceda56f6bfa700c375496
Mahmoud Abdel Latif <mah008@me.com>
2020-12-22 10:13:48 +0200

    Update odoo_pro18.sh

--- odoo_pro18.sh
+++ odoo_pro18.sh
@@ -34,8 +34,8 @@ IS_ENTERPRISE="True"
 ## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
 ## in order to have correct version of wkhtmltox installed, for a danger note refer to 
 ## https://www.odoo.com/documentation/8.0/setup/install.html#deb ):
-WKHTMLTOX_X64=https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
-WKHTMLTOX_X32=https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.bionic_i386.deb
+WKHTMLTOX_X64=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+WKHTMLTOX_X32=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_i386.deb
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
