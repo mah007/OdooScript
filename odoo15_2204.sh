01e296e82a2459eabd2f51c43ee2d89efbfba8a0
Mahmoud Abdel Latif <mah008@me.com>
2024-02-16 22:27:29 +0200

    Update odoo15_2204.sh
    done

--- odoo15_2204.sh
+++ odoo15_2204.sh
@@ -107,8 +107,7 @@ if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
   fi
   sudo wget $_url
   sudo gdebi --n `basename $_url`
-  sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
-  sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
+
 else
   echo "Wkhtmltopdf isn't installed due to the choice of the user!"
 fi
