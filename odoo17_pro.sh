a352f5f98adba8dc25a3eb3a99e7013cc3783523
Mahmoud Abdel Latif <mah008@me.com>
2024-06-01 01:29:28 +0300

    Update odoo17_pro.sh

--- odoo17_pro.sh
+++ odoo17_pro.sh
@@ -89,7 +89,7 @@ echo -e "\n--- Install other required packages"
 sudo apt-get install node-clean-css -y
 sudo apt-get install node-less -y
 sudo apt-get install python-gevent -y
-sudo apt-get install libcairo2-dev python3-cairo
+sudo apt-get install libcairo2-dev python3-cairo -y
 pip3 install rlPyCairo
 apt-get install libwww-perl -y
 
